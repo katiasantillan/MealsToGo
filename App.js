c5f2468f5e55724d4fa29f34f8c6427cab154067
katiasantillan <ksantillana1400@alumno.ipn.mx>
2021-03-30 18:44:03 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
+import * as firebase from "firebase";
+
+import { Navigation } from "./src/infrastructure/navigation";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      firebase;
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("katia@gmail.com", "20")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
